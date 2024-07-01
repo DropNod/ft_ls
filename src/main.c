c57f2ec7a06769b599c611eed3225717fdae58b7
DropNod <prigentgwechen@gmail.com>
2024-07-01 15:14:43 +0200

    + | Display improvement, added dir struct

--- src/main.c
+++ src/main.c
@@ -1,17 +1,54 @@
 #include "ft_ls.h"
 
-int main(int ac, char **av)
+typedef struct dir_s
+{
+  struct dirent **entrys;
+  int nb_entrys;
+} dir_t;
+
+int count_entrys(const char *dir_name)
+{
+  struct dirent *file;
+  int output;
+  DIR *dir;
+
+  dir = opendir(dir_name);
+  file = readdir(dir);
+  output = 0;
+  while (file)
+  {
+    output++;
+    file = readdir(dir);
+  }
+  closedir(dir);
+  return (output);
+}
+
+dir_t init_dir(const char *dir_name)
 {
   DIR *dir;
+  dir_t output;
+
+  output.nb_entrys = count_entrys(dir_name);
+  output.entrys = malloc(output.nb_entrys * sizeof(struct dirent *));
+  dir = opendir(dir_name);
+  for (int i = 0; i < output.nb_entrys; i++)
+    output.entrys[i] = readdir(dir);
+  closedir(dir);
+  return (output);
+}   
+
+int main(int ac, char **av)
+{
+  dir_t dir;
 
   (void) ac;
   (void) av;
-  dir = opendir(".");
-  struct dirent *file = readdir(dir);
-  while (file)
+  dir = init_dir(".");
+  for (int i = 0; i < dir.nb_entrys; i++)
   {
-    printf("%s\n", file->d_name);
-    file = readdir(dir);
+    if (dir.entrys[i]->d_name[0] != '.')
+      printf("%s  ", dir.entrys[i]->d_name);
   }
   return (EXIT_SUCCESS);
 }
